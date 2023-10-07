ed9f3404b9a8685fec6a39f991fe2fe88e6222b3
costapapa <cost.papa87@gmail.com>
2023-10-07 23:59:07 +0100

    player go's added and win conditions

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,7 +5,42 @@ export const Board = () => {
   const [xisNext, setXisNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function calculateWinner(squares: any[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next Player: " + (xisNext ? "X" : "0");
+  }
+
   function handleClick(i: number) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
     nextSquares[i] = xisNext ? "X" : "O";
     setSquares(nextSquares);
@@ -14,6 +49,7 @@ export const Board = () => {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} square={() => handleClick(0)} />
         <Square value={squares[1]} square={() => handleClick(1)} />
